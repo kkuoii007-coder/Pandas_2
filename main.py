a72d01921d1fe0cf75966217182ec3901ea89386
kkuoii007-coder <kkuoii007@gmail.com>
2026-03-08 22:47:47 +0300

    make_main

---
+++ main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+students = [
+    {'name': 'Анна Иванова', 'math': 5, 'physics': 4, 'chemistry': 5, 'russian': 5, 'english': 4},
+    {'name': 'Борис Петров', 'math': 4, 'physics': 5, 'chemistry': 4, 'russian': 4, 'english': 5},
+    {'name': 'Виктория Сидорова', 'math': 5, 'physics': 5, 'chemistry': 5, 'russian': 5, 'english': 5},
+    {'name': 'Глеб Кузнецов', 'math': 3, 'physics': 4, 'chemistry': 3, 'russian': 4, 'english': 3},
+    {'name': 'Анна Иванова', 'math': 5, 'physics': 4, 'chemistry': 5, 'russian': 5, 'english': 4},
+    {'name': 'Борис Петров', 'math': 4, 'physics': 5, 'chemistry': 4, 'russian': 4, 'english': 5},
+    {'name': 'Глеб Кузнецов', 'math': 5, 'physics': 5, 'chemistry': 5, 'russian': 5, 'english': 5},
+    {'name': 'Глеб Кузнецов', 'math': 3, 'physics': 4, 'chemistry': 3, 'russian': 4, 'english': 3},
+    {'name': 'Арсений Петров', 'math': 3, 'physics': 3, 'chemistry': 3, 'russian': 5, 'english': 3},
+    {'name': 'Юрий Юнусов', 'math': 4, 'physics': 4, 'chemistry': 4, 'russian': 4, 'english': 4},
+    {'name': 'Маша Васнецова', 'math': 5, 'physics': 5, 'chemistry': 5, 'russian': 5, 'english': 5}
+    ]
+
